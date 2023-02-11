1d29993700ee994118beb230b1b63660c8f48a54
Bruno Eilliar <bruno.eilliar@afterverse.com>
2023-02-11 15:13:54 -0300

    Add Class example

---
+++ lec_20/Cat.h
@@ -0,0 +1,13 @@
+#ifndef CAT_H_INCLUDED
+#define CAT_H_INCLUDED
+
+class Cat
+{
+private:
+    /* data */
+public:
+    void speak();
+    void jump();
+};
+
+#endif
---
+++ lec_20/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "Cat.h"
+
+using namespace std;
+
+
+int main(){
+
+    Cat myCat;
+
+    myCat.speak();
+    myCat.jump();
+
+    return 0;
+}
