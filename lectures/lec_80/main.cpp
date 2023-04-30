2d0598f79800bf5167a8f60d02fadb6fb986dd44
Eilliar <engbrunoeilliar@gmail.com>
2023-04-30 00:04:52 -0300

    Add polymorphism example

---
+++ lectures/lec_80/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+class Animal
+{
+public:
+    Animal(){};
+    virtual ~Animal(){};
+    virtual void speak(){
+        cout << "????????????" << endl;
+    };
+};
+ 
+class Dog: public Animal {
+public:
+    Dog(){};
+    virtual ~Dog(){};
+    virtual void speak(){
+        cout << "rrRRrrrRRRrrrRRR" << endl;
+    }
+
+    void jump(){
+        cout << "dog just jumped!" << endl;
+    }
+};
+
+int main(){
+
+    Animal *pDog = new Dog();
+
+    pDog->speak();
+    // This yield error: 'class Animal' has no member named 'jump'
+    // pDog->jump();
+
+    delete pDog;
+
+    return 0;
+}
